2e0bba100d91139be17ce6c9c1acb193a2410cf6
magi <mpayeras@cifpfbmoll.eu>
2022-01-18 19:21:54 +0100

    repte 5

---
+++ DWEC/reptes/repte 3/js/main.js
@@ -0,0 +1,16 @@
+function dividirCadena(cadena) {
+  var array = cadena.split(",");
+
+  for (var i = 0; i < array.length; i++) {
+    document.write(array[i] + " / ");
+  }
+
+  document.getElementById("resultat").innerHTML = ` 
+ 
+  for (var i = 0; i < array.length; i++) {
+    document.write(array[i] + " / ");
+  }
+  
+  
+  `;
+}
